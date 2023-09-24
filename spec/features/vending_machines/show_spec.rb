bff6139c7c654183bb9a48b27843388f9a5132d0
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-24 11:29:27 -0600

    user story 1

--- spec/features/vending_machines/show_spec.rb
+++ spec/features/vending_machines/show_spec.rb
@@ -1,12 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe 'When a user visits a vending machine show page', type: :feature do
+  before(:each) do
+    @owner = Owner.create(name: "Sam's Snacks")
+    @dons = @owner.machines.create(location: "Don's Mixed Drinks")
+
+    @burger = @dons.snacks.create(name: "White Castle Burger", price: 3.50)
+    @pop_rocks = @dons.snacks.create(name: "Pop Rocks", price: 1.50)
+    @cheetos = @dons.snacks.create(name: "Flaming Hot Cheetos", price: 2.50)
+
+    visit machine_path(@dons)
+  end
   scenario 'they see the location of that machine' do
-    owner = Owner.create(name: "Sam's Snacks")
-    dons  = owner.machines.create(location: "Don's Mixed Drinks")
-
-    visit machine_path(dons)
-
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
+  end
+
+  scenario "I see the name of all of the snacks associated with that vending machine along with their price" do
+    expect(page).to have_content("Snacks")
+
+    within("#snacks") do
+      expect(page).to have_content("White Castle Burger: $3.50")
+      expect(page).to have_content("Pop Rocks: $1.50")
+      expect(page).to have_content("Flaming Hot Cheetos: $2.50")
+    end
+  end
+
+  scenario "I see the average price for all of the snacks in that machine" do
+    expect(page).to have_content("Average Price: $2.50")
   end
 end
--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -7,4 +7,19 @@ RSpec.describe Machine, type: :model do
     it { should have_many :machine_snacks }
     it { should have_many(:snacks).through(:machine_snacks)}
   end
+
+  before(:each) do
+    @owner = Owner.create(name: "Sam's Snacks")
+    @dons = @owner.machines.create(location: "Don's Mixed Drinks")
+
+    @burger = @dons.snacks.create(name: "White Castle Burger", price: 3.50)
+    @pop_rocks = @dons.snacks.create(name: "Pop Rocks", price: 1.50)
+    @cheetos = @dons.snacks.create(name: "Flaming Hot Cheetos", price: 2.50)
+  end
+
+  describe "#average_price" do
+    it "should return the average price of all snacks for a given machine" do
+      expect(@dons.average_price).to eq(2.50)
+    end
+  end
 end
